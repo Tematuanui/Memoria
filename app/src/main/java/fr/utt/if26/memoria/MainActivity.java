22f3eb8800e7bb895c402f5991ddedd4f0e99cfd
Tematuanui <temah01@gmail.com>
2019-11-07 15:22:57 +0100

    creations des themes

--- app/src/main/java/fr/utt/if26/memoria/MainActivity.java
+++ app/src/main/java/fr/utt/if26/memoria/MainActivity.java
@@ -1,13 +1,67 @@
 package fr.utt.if26.memoria;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+MediaPlayer bip;
+MediaPlayer miiSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bip = MediaPlayer.create(getApplicationContext(), R.raw.bip);
+        miiSong = MediaPlayer.create(getApplicationContext(),R.raw.miisong);
+
+        miiSong.start();
+        miiSong.setLooping(true);
+
+        final Button bt_jouer = (Button)findViewById(R.id.main_bt_jouer);
+        final Button bt_options = (Button)findViewById((R.id.main_bt_options));
+        final Button bt_scores = (Button)findViewById(R.id.main_bt_scores);
+        final Button bt_quitter = (Button)findViewById(R.id.main_bt_quitter);
+
+        final Intent itt_jouer = new Intent(MainActivity.this,JouerActivity.class);
+        final Intent itt_options = new Intent(MainActivity.this,OptionsActivity.class);
+        final Intent itt_scores = new Intent(MainActivity.this,ScoresActivity.class);
+
+        bt_jouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bip.start();
+                startActivity(itt_jouer);
+            }
+        });
+
+        bt_scores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bip.start();
+                startActivity(itt_scores);
+            }
+        });
+
+        bt_options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bip.start();
+                startActivity(itt_options);
+            }
+        });
+
+        bt_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/fr/utt/if26/memoria/OptionsActivity.java
@@ -0,0 +1,85 @@
+package fr.utt.if26.memoria;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionsActivity extends AppCompatActivity {
+MediaPlayer bip;
+final Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        bip = MediaPlayer.create(getApplicationContext(),R.raw.bip);
+
+
+         final Button bt_theme = (Button)findViewById(R.id.options_bt_theme);
+         final Button bt_audio = (Button)findViewById(R.id.options_bt_audio);
+         final Button bt_retour = (Button)findViewById(R.id.options_bt_retour);
+
+        bt_theme.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                bip.start();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setTitle("CHOIX DE THEME");
+
+                alertDialogBuilder
+                        .setMessage("Choisissez votre theme")
+                        .setCancelable(false)
+
+                        .setPositiveButton("NORMAL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                bip.start();
+
+
+                            }
+                        })
+
+                        .setPositiveButton("DARK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                bip.start();
+
+                            }
+                        })
+
+
+                        .setNeutralButton("RETOUR", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                bip.start();
+                                dialog.cancel();
+
+                            }
+                        });
+
+                AlertDialog alertDialog_theme = alertDialogBuilder.create();
+
+                alertDialog_theme.show();
+            }
+        });
+    }
+
+    public void setStyle (){
+final Button bt = (Button) findViewById(R.id.options_bt_audio);
+
+final Button b1 = new Button(new ContextThemeWrapper(OptionsActivity.this,R.style.WonderButton));
+
+    }
+
+}
+
